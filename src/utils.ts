a32c64d2f1b0913cc012db07912e06c53306d63a
djh20 <danmanmrdj@gmail.com>
2022-07-17 21:28:43 +1200

    Add util random functions

--- src/utils.ts
+++ src/utils.ts
@@ -13,3 +13,11 @@ export function map(value: number, fromMin: number, fromMax: number, toMin: numb
   const mapped: number = ((value - fromMin) * (toMax - toMin)) / (fromMax - fromMin) + toMin;
   return clamp(mapped, toMin, toMax);
 }
+
+export function getRandomElement<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+export function getRandomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
